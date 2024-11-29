fab1f53fe6ae47f09c97f90074abf3598a1a816e
NGUYEN THANH TAI <taing1710@gmail.com>
2024-11-29 20:43:35 +0700

    #80 - Update RoomByPrice

--- src/main/java/com/poly/controller/DiscountController.java
+++ src/main/java/com/poly/controller/DiscountController.java
@@ -3,6 +3,7 @@ package com.poly.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +20,7 @@ import com.poly.util._enum.RoomStatus;
 
 @Controller
 @RequestMapping("/discounts")
+@PreAuthorize("hasAuthority('ADMIN')")
 public class DiscountController {
 	  @Autowired
 	    private DiscountService discountService;
